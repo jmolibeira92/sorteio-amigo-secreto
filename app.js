85c4db61235b0bfa8e0badc5ddf01461ccbb26ca
João Marcelo <jmbo1992@hotmail.com>
2025-01-19 18:27:34 -0300

    Sortear Amigos

--- app.js
+++ app.js
@@ -3,13 +3,38 @@ let amigos = [];
 
 function adicionarAmigo() {
     let amigoIncluir = document.querySelector('input').value;
-    console.log(amigoIncluir);
-    if (amigoIncluir == ""){
+    if (amigoIncluir == ''){
         alert('Por favor, insira um nome.');
-
+        amigoIncluir = '';
     }
     else{
-        amigos = amigoIncluir;
+        amigos.push(amigoIncluir);
+        console.log(amigos);
+        limparCampo();
     }
+}
+
+function sortearAmigo() {
+    if (amigos == ''){
+        alert('Por favor, insira um nome para ser sorteado.');
+    }
+    else {
+    let quantidadeAmigos = amigos.length;
+    let amigoSorteado = parseInt(Math.random() * quantidadeAmigos);
+    console.log(amigoSorteado);
+    console.log(amigos[amigoSorteado]);
+    exibirTextoSorteado('p', 'O nome do amigo sorteado é: ' + amigos[amigoSorteado]);
+    }
+}
+
+function exibirTextoSorteado(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function limparCampo() {
+    amigoIncluir = document.querySelector('input');
+    amigoIncluir.value = '';
 }
 
