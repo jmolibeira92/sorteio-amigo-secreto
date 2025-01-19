ba0f580d65567284cb8f6278ac610171edebe3a5
João Marcelo <jmbo1992@hotmail.com>
2025-01-19 16:43:53 -0300

    Atualizar Lista Amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigoIncluir = document.querySelector('input').value;
+    console.log(amigoIncluir);
+    if (amigoIncluir == ""){
+        alert('Por favor, insira um nome.');
+
+    }
+    else{
+        amigos = amigoIncluir;
+    }
+}
+
